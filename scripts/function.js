d6bd5866898b1a57faed425e3a0b32d62fb9be18
Deep Patel <deep.ag123007@gmail.com>
2019-04-15 14:53:39 -0500

    ch4 cookout is done

--- scripts/function.js
+++ scripts/function.js
@@ -41,9 +41,49 @@ const under = takeNumber("Underdogs")
 console.log(under) 
 
 
+//Practice: Cookout
 
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
 
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
 
+const cookedFood = [];
+
+function grill (currentObject) {
+    currentObject.cooked = true;
+
+    cookedFood.push(currentObject);
+};
+
+foods.forEach(food =>{
+    grill(food);
+})
+
+console.log("cookedfood",cookedFood)
 
 
 
